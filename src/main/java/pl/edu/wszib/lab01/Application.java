c2a57c342a0b955e64773f5f139cf0f599781c2c
Piotr Tomasz Nuciński <nucinski@student.wszib.edu.pl>
2022-10-01 16:40:35 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/lab01/Application.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.lab01;
+
+public class Application {
+    public static void main(String[] args) {
+        MyFirstClass myFirstClass = new MyFirstClass();
+        myFirstClass.FirstField2 = "Test";
+
+
+    }
+}
---
+++ src/main/java/pl/edu/wszib/lab01/MyFirstClass.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.lab01;
+
+public class MyFirstClass {
+
+    private String firstField;
+    String FirstField2;
+    // modyfikatory dostępu
+    // private
+    //
+    // protected
+    // public
+
+    public void run(){
+        System.out.println("FirstField = " + firstField);
+    }
+}
